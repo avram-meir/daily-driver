13cefcd6102fd6abce6833d12bca144a927cf6fa
Adam Allgood <avram.meir@gmail.com>
2022-05-26 17:04:00 -0400

    Add remaining code - no errors when passed no args or just -b and -d args - needs further testing

--- daily-driver.sh
+++ daily-driver.sh
@@ -77,10 +77,16 @@ if [ $? -ne 0 ] ; then
 	exit 1
 fi
 
+if [ $startdate -gt $stopdate ] ; then
+	tempdate=$enddate
+	enddate=$startdate
+	startdate=$tempdate
+fi
+
 # --- Add backfilling if provided ---
 
 if ! [ -z "$back" ] ; then
-	echo "Backdating provided!"
+
 	startdate=$(date +%Y%m%d --date "$startdate $back")
 
 	if [ $? -ne 0 ] ; then
@@ -91,12 +97,81 @@ if ! [ -z "$back" ] ; then
 
 fi
 
-echo "${back}"
-echo "${startdate}"
-echo "${stopdate}"
-echo "${config}"
+# --- Get archive information from config file ---
 
-printf "Hello world!\n"
+alwaysupdate=0
+
+if [ -z $config ] ; then    # Always update if no config file was supplied
+	alwaysupdate=1
+elif [ -s $config ] ; then  # Config file supplied
+
+	# --- Execute the file as a bash script to pull in expected variables ---
+
+	. $config
+
+	if [ -z "$archive" ] ; then
+		printf "No archive parameter found in %s\n" $config >&2
+		alwaysupdate=1
+	fi
+
+	if [ -z "$files" ] ; then
+		printf "No files parameter found in %s\n" $config >&2
+		alwaysupdate=1
+	fi
+
+else
+	printf "%s is an empty file or does not exist\n" $config >&2
+	usage >&2
+	exit 1
+fi
+
+# --- Loop through all days in the range defined by startdate and enddate ---
+
+printf "Scanning and updating archive from %s to %s\n" $startdate $stopdate
+
+cd $(dirname "$0")
+
+failed=0
+date=$startdate
+
+until [ $date -gt $stopdate ] ; do
+	update=0
+
+	if [ $alwaysupdate -eq 1 ] ; then
+		update=1
+	else
+
+		# --- Scan archive for missing files ---
+
+		for fil in "${files[@]}" ; do
+			filename="${archive}/${fil}"
+			filename=$(date +"${filename}" --d "${date}")
+
+			if ! [ -s $filename ] ; then
+				update=1
+			fi
+			
+		done
+
+	fi
+
+	if [ $update -eq 1 ] ; then
+		printf "Updating archive for %s\n" $date
+
+		# ********************************
+		#                                *
+		# Do your wonderful stuff here!  *
+		#                                *
+		# ********************************
+
+		# Do something with $failed if things go wrong
+
+	else
+		printf "Archive complete for %s\n" $date
+	fi
+
+	date=$(date +%Y%m%d --date "$date+1day")
+done
 
 exit 0
 
